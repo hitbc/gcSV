5665deade1b4e1da9eacb4e51d18a8c94bcebeea
Gaoyang Li <gaoyangli@hit.edu.cn>
2026-01-07 15:55:16 +0800

    Delete src directory

--- src/clib/desc.h
+++
@@ -1,4 +0,0 @@
-#define PACKAGE_NAME "gcSV"
-#define PACKAGE_VERSION "1.0.0"
-#define CONTACT "Gaoyang Li <gaoyangli@hit.edu.cn>"
-#define DEBUG 1
--- src/htslib/config.h
+++
@@ -1,3 +0,0 @@
-//#define HAVE_LIBBZ2 1
-#define HAVE_FSEEKO 1
-#define HAVE_DRAND48 1
